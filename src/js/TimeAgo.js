a48f3dc3c949b095ceda3a48e2d0a2d49c63d3e3
HAZberi <mhazuberi@gmail.com>
2020-12-21 21:33:19 -0700

    time ago tested implementation

--- src/js/TimeAgo.js
+++ src/js/TimeAgo.js
@@ -1,48 +1,45 @@
 const timeAgo = function (date) {
   const timeStamp = new Date(date);
   const timeElapsed = new Date() - timeStamp;
-  console.log(timeStamp);
-  console.log(timeElapsed);
   const timeElapsedInSeconds = timeElapsed / 1000;
   const timeElapsedInMinutes = timeElapsedInSeconds / 60;
   const timeElapsedInHours = timeElapsedInMinutes / 60;
   const timeElapsedInDays = timeElapsedInHours / 24;
-  const timeElapsedInMonths = timeElapsedInDays / 12;
-  const timeElapsedInYears = timeElapsedInYears / 365;
+  const timeElapsedInMonths = timeElapsedInDays / 30.42;
+  const timeElapsedInYears = timeElapsedInMonths / 12;
   if (timeElapsedInYears > 1) {
-    return `${Math.floor(timeElapsedInYears)} ${
+    return `${Math.round(timeElapsedInYears)} ${
       timeElapsedInYears < 2 ? 'year ago' : 'years ago'
     }`;
   }
   if (timeElapsedInMonths > 1 && timeElapsedInMonths < 13) {
-    return `${Math.floor(timeElapsedInMonths)} ${
+    return `${Math.round(timeElapsedInMonths)} ${
       timeElapsedInMonths < 2 ? 'month ago' : 'months ago'
     }`;
   }
   if (timeElapsedInDays > 1 && timeElapsedInDays < 31) {
-    return `${Math.floor(timeElapsedInDays)} ${
+    return `${Math.round(timeElapsedInDays)} ${
       timeElapsedInDays < 2 ? 'day ago' : 'days ago'
     }`;
   }
   if (timeElapsedInHours > 1 && timeElapsedInHours < 24) {
-    return `${Math.floor(timeElapsedInHours)} ${
+    return `${Math.round(timeElapsedInHours)} ${
       timeElapsedInHours < 2 ? 'hour ago' : 'hours ago'
     }`;
   }
   if (timeElapsedInMinutes > 1 && timeElapsedInMinutes < 60) {
-    return `${Math.floor(timeElapsedInMinutes)} ${
+    return `${Math.round(timeElapsedInMinutes)} ${
       timeElapsedInMinutes < 2 ? 'minute ago' : 'minutes ago'
     }`;
   }
   if (timeElapsedInSeconds > 1 && timeElapsedInSeconds < 60) {
-    return `${Math.floor(timeElapsedInSeconds)} ${
+    return `${Math.round(timeElapsedInSeconds)} ${
       timeElapsedInSeconds < 2 ? 'second ago' : 'seconds ago'
     }`;
   }
 };
 
-export default timeAgo;  
-
+export default timeAgo;
 
 //Notes:
 //The time ago function takes a date string in the past.
