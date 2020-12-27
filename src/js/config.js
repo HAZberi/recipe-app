3be1c38989d672c9ee9f6bd4b6b2d7353cb8d435
HAZberi <mhazuberi@gmail.com>
2020-12-26 20:12:32 -0700

    API KEY update

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const API_TIMEOUT = 10;
 export const RESULTS_PER_PAGE = 10;
-export const API_KEY = "6d3db235-e538-465d-a436-f128c640bd9a";
+export const API_KEY = "14408e7f-8c3b-400c-bc90-a74a8520cf33";
 export const MODAL_WINDOW_TIMEOUT = 7;
 export const MODAL_WINDOW_ANIMATION_TIMEOUT = 0.75;
